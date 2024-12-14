6771e427e7b87c41481121364d92f4ea68282bcb
MashooqAli9140 <Faizsde9140@gmail.com>
2024-12-14 17:04:49 +0530

    cdnjs link and heart pulse icon added

--- src/componants/Navbar.jsx
+++ src/componants/Navbar.jsx
@@ -3,7 +3,8 @@ import React from 'react'
 const Navbar = () => {
   return (
     <div style={{ backgroundColor:'lightblue', padding:"10px 150px 10px 150px"}}>
-          <h1> Act 4 fitness </h1>     
+          <h1> Act 4 fitness </h1>
+          <i class="fa-solid fa-heart-pulse"></i>     
     </div>
   )
 }
