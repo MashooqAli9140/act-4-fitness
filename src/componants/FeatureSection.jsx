48886a6904cd9aa0d46bc20fc82d3331695ac3a8
MashooqAli9140 <Faizsde9140@gmail.com>
2024-12-14 18:53:42 +0530

    form created

--- src/componants/FeatureSection.jsx
+++ src/componants/FeatureSection.jsx
@@ -2,9 +2,13 @@ import React from 'react'
 
 const FeatureSection = () => {
   return (
-    <div style={{ backgroundColor:"red"}}>
-         <div id='formcard' style={{backgroundColor:"white", maxWidth:"400px"}}>
-              <input type="text" placeholder='Please enter your name' />
+    <div style={{ display:"flex", justifyContent:"space-evenly", gap:"20px", flexWrap:"wrap", alignContent:"center" ,alignItems:"center", backgroundColor:"red", padding:"20px 20px 20px 20px"}}>
+         <div id='formcard' style={{ padding:"20px 20px 20px 20px", borderRadius:"25px" ,backgroundColor:"white", width:"400px", maxWidth:"600px"}}>
+               <label style={{ display:"block"}}>Name</label>
+              <input style={{ width:"100%", border:"1px solid grey", borderRadius:"25px", padding:"10px 10px 10px 10px"}} type="text" placeholder='Please enter your name' />
+         </div>
+         <div>
+            <button> Get Started </button>
          </div>
     </div>
   )
