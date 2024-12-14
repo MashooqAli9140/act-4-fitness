f10ad624016fb1314b8ec2b256cd5e09f1142110
MashooqAli9140 <Faizsde9140@gmail.com>
2024-12-14 18:29:47 +0530

    navbar css updated

--- src/componants/Navbar.jsx
+++ src/componants/Navbar.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import '/src/index.css'
+
 const Navbar = () => {
   return (
-    <div id='navbardiv' style={{ backgroundColor:"#F5FEFF", padding:"10px 10px 10px 10px", flexWrap:"wrap" , display:"flex",alignContent:"center", alignItems:"center", justifyContent:"space-evenly"}}>
+    <div id='navbardiv' style={{ backgroundColor:"#E5FEFF", padding:"10px 10px 10px 10px", flexWrap:"wrap" , display:"flex",alignContent:"center", alignItems:"center", justifyContent:"space-evenly"}}>
 
           <h1 style={{ color:"#0A2751"}}> ACT <i class="fa-solid fa-heart-pulse" style={{ color:"red"}}></i>  4 FITNESS </h1>
           <div style={{ gap:"10px", display:"flex", justifyContent:"center", alignContent:"center", alignItems:"center" }}>
