cd5770d967a865938a8a8ae88ac9c169f3012379
MashooqAli9140 <Faizsde9140@gmail.com>
2024-12-14 17:01:49 +0530

    add some style in navbar div

--- src/componants/Navbar.jsx
+++ src/componants/Navbar.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div>
+    <div style={{ backgroundColor:'lightblue', padding:"10px 150px 10px 150px"}}>
           <h1> Act 4 fitness </h1>     
     </div>
   )
