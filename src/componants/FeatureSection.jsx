c73af8dd064ddf2c3efa715a837459d29b29a441
MashooqAli9140 <Faizsde9140@gmail.com>
2024-12-14 18:43:08 +0530

    feature section added

---
+++ src/componants/FeatureSection.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const FeatureSection = () => {
+  return (
+    <div style={{ backgroundColor:"red"}}>
+         <div id='formcard' style={{backgroundColor:"white", maxWidth:"400px"}}>
+              <input type="text" placeholder='Please enter your name' />
+         </div>
+    </div>
+  )
+}
+
+export default FeatureSection
